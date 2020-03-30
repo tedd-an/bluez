cf83a1f438c26f4003e2cf40ec1f80da2c827f47
Howard Chung <howardchung@google.com>
2020-03-30 16:03:57 +0800

    tools/btmgmt: Add setting string for LE Set Advertising Interval
    $ btmgmt info
        current settings: powered bondable ... advertising-intervals
    
    Signed-off-by: Howard Chung <howardchung@google.com>

--- tools/btmgmt.c
+++ tools/btmgmt.c
@@ -355,6 +355,7 @@ static const char *settings_str[] = {
 				"static-addr",
 				"phy-configuration",
 				"wide-band-speech",
+				"advertising-intervals",
 };
 
 static const char *settings2str(uint32_t settings)
