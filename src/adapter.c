efe7edfaef084e83e628ea1ea1d66c023b3fa4d9
Joseph Hwang <josephsih@chromium.org>
2020-08-20 23:49:27 -0700

    adapter: remove eir_data.flags in device_update_last_seen()
    Bluez has difficulty in pairing with Apple Airpods. This issue is
    caused by the incorrect selection of BD address type due to two factors:
    
    (1) The LE advertising reports emitted by Airpods do not carry eir
        data flags.
    (2) If the eir data flags is missing, the found device would not be
        considered as coming with bredr address for some historical
        obsolete reason.
    
    This patch fixes (2) above.
    
    Tested on Chrome OS by pairing with Airpods.
    Without this patch, the pairing success rate is about 20%.
    With this patch, the pairing success rate becomes almost 100%.

--- src/adapter.c
+++ src/adapter.c
@@ -6642,14 +6642,8 @@ static void update_found_devices(struct btd_adapter *adapter,
 
 	device_update_last_seen(dev, bdaddr_type);
 
-	/*
-	 * FIXME: We need to check for non-zero flags first because
-	 * older kernels send separate adv_ind and scan_rsp. Newer
-	 * kernels send them merged, so once we know which mgmt version
-	 * supports this we can make the non-zero check conditional.
-	 */
-	if (bdaddr_type != BDADDR_BREDR && eir_data.flags &&
-					!(eir_data.flags & EIR_BREDR_UNSUP)) {
+	if (bdaddr_type != BDADDR_BREDR &&
+			!(eir_data.flags & EIR_BREDR_UNSUP)) {
 		device_set_bredr_support(dev);
 		/* Update last seen for BR/EDR in case its flag is set */
 		device_update_last_seen(dev, BDADDR_BREDR);
